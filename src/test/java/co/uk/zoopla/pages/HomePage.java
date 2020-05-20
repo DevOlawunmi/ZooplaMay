ea6abdf37904e5f4509d6cc493bc7d02671b7104
Ola <olawunmi263@yahoo.com>
2020-05-20 05:14:54 +0100

    Homepage Created

--- src/test/java/co/uk/zoopla/pages/HomePage.java
+++ src/test/java/co/uk/zoopla/pages/HomePage.java
@@ -1,6 +1,8 @@
 package co.uk.zoopla.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
@@ -8,4 +10,18 @@ public class HomePage extends BasePage {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+    @FindBy (id = "search-input-location")
+    private WebElement searchField;
+    @FindBy (id = "forsale_price_min")
+    private WebElement minPrice;
+    @FindBy(id = "forsale_price_max")
+    private WebElement maxPrice;
+    @FindBy (id = "property_type")
+    private WebElement propertyType;
+    @FindBy (id = "beds_min")
+    private WebElement noOfBeds;
+    @FindBy(id = "search-submit")
+    private WebElement searchButton;
+    @FindBy (className = "ui-button-primary ui-cookie-accept-all-medium-large")
+    private WebElement acceptCookies;
 }
